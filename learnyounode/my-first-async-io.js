1f690524af10f34fa2b7d4264b8d696f09c5d8dc
DeAndre Boston <dboston3@yahoo.com>
2020-02-17 14:51:21 -0600

    Async solution

--- learnyounode/my-first-async-io.js
+++ learnyounode/my-first-async-io.js
@@ -1,18 +1,11 @@
 const fs = require('fs');
+const file = process.argv[2];
 
-const contents = fs.readFile;
+fs.readFile(file, function(err, contents) {
+  if (err) {
+    return console.log(err);
+  }
 
-function callback(err, data) {
-  /* ... */
-}
-
-function processData(callback) {
-  fetchData(function(err, data) {
-    if (err) {
-      console.log('An error has occurred. Abort everything!');
-      return callback(err);
-    }
-    data += 1;
-    callback(data);
-  });
-}
+  const lines = contents.toString().split('\n').length - 1;
+  console.log(lines);
+});
