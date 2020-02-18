b63d42630f63f14c6f24c9f6dd2383bf7eed98de
DeAndre Boston <dboston3@yahoo.com>
2020-02-18 11:31:43 -0600

    Add comment explanations

--- learnyounode/filtered-ls.js
+++ learnyounode/filtered-ls.js
@@ -1,4 +1,6 @@
+//allows you to use node file system methods
 const fs = require('fs');
+//this method returns the extension of a path.
 const path = require('path');
 
 const folder = process.argv[2];
@@ -8,7 +10,8 @@ fs.readdir(folder, function(err, files) {
   if (err) {
     return console.log(err);
   }
-
+  /*loops through each file name and check if the file extension name equals 
+  ext arguament passed into the console. If it does print it to the console */
   files.forEach(function(file) {
     if (path.extname(file) === ext) {
       console.log(file);
